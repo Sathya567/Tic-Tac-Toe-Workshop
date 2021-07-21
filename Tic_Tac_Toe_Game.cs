1d584bc3bb2efa2585e03cd2bd40e9bdcf44afe0
BootCampBL <satyabhandari43@gmail.com>
2021-07-21 14:28:09 +0530

    [sathya] Added- Player input and comp input taken

---
+++ Tic_Tac_Toe_Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Tic_Tac_Toe_Game
+    {
+        public const char X = 'X';
+        public const char O = 'O';
+        public void gameBoard()
+        {
+            char[] gameBoard = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+            Console.WriteLine(gameBoard[2] + " " + gameBoard[3] + " " + gameBoard[4]);
+            Console.WriteLine("-------------");
+            Console.WriteLine(gameBoard[5] + " " + gameBoard[6] + " " + gameBoard[7]);
+            Console.WriteLine("-------------");
+            Console.WriteLine(gameBoard[8] + " " + gameBoard[9] + " " + gameBoard[10]);
+        }
+        public void abillityToInput()
+        {
+            Random random = new Random();
+            int compChoice = random.Next(0, 2);
+            Console.WriteLine("Enter you Choice from O/X :");
+            char playerChoice = Convert.ToChar(Console.ReadLine());
+            if (playerChoice == 'X' || playerChoice == 'O')
+            {
+                Console.WriteLine("Player Chose " + playerChoice);
+            }
+            else
+            {
+                Console.WriteLine("Entered invalid input  ");
+            }
+
+            switch (compChoice)
+            {
+                case 0:
+                    Console.WriteLine("Comp have choose " + O);
+                    break;
+                case 1:
+                    Console.WriteLine("Comp have choose " + X);
+                    break;
+            }
+
+        }
+    }
+}
+  
+  
